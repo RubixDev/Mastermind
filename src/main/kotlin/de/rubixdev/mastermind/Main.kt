b90036b47f826c3c54153859a87b3a5e8bd3fc4b
RubixDev <silas.groh@t-online.de>
2021-07-17 13:57:29 +0200

    Change line endings from CRLF to LF

--- src/main/kotlin/de/rubixdev/mastermind/Main.kt
+++ src/main/kotlin/de/rubixdev/mastermind/Main.kt
@@ -80,7 +80,7 @@ object Constants {
  */
 @KordPreview
 suspend fun main() {
-    client = Kord(object {}.javaClass.getResource("/devToken.txt")!!.readText()) {
+    client = Kord(object {}.javaClass.getResource("/token.txt")!!.readText()) {
         intents = Intents(
             Intent.Guilds,
             Intent.GuildMessageReactions
