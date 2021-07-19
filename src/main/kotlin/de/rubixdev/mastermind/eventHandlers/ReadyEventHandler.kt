295b60e8557331361512ca8e033b9074ba71d821
RubixDev <silas.groh@t-online.de>
2021-07-19 14:00:18 +0200

    Bulk create commands

--- src/main/kotlin/de/rubixdev/mastermind/eventHandlers/ReadyEventHandler.kt
+++ src/main/kotlin/de/rubixdev/mastermind/eventHandlers/ReadyEventHandler.kt
@@ -1,14 +1,12 @@
 package de.rubixdev.mastermind.eventHandlers
 
-import de.rubixdev.mastermind.displayGuilds
-import de.rubixdev.mastermind.isReady
-import de.rubixdev.mastermind.testCommandIds
-import de.rubixdev.mastermind.updatePresence
+import de.rubixdev.mastermind.*
 import dev.kord.common.annotation.KordPreview
 import dev.kord.common.entity.Snowflake
-import dev.kord.core.behavior.createApplicationCommand
+import dev.kord.core.behavior.createApplicationCommands
 import dev.kord.core.event.gateway.ReadyEvent
-import dev.kord.rest.builder.interaction.ApplicationCommandCreateBuilder
+import dev.kord.rest.builder.interaction.ApplicationCommandsCreateBuilder
+import kotlinx.coroutines.flow.collect
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 
@@ -19,23 +17,25 @@ suspend fun ReadyEvent.handleReadyEvent() {
     updatePresence()
     logger.info("Bot currently on guilds:${displayGuilds()}")
 
-    addCommand("show", "Shows current board or starts a new game if previous one is finished")
-    addCommand("newgame", "Cancels the current game and starts a new one")
-    addCommand("rules", "Explains the game and commands")
-    addCommand("invite", "Displays an invite link for this bot")
-    addCommand("help", "Alias for /rules")
-    addCommand("config", "Displays a config screen")
-    addCommand("setpins", "Sets the amount of pins you want to play with") {
-        int("amount", "Amount of pins. Value between 3 and 6") {
-            required = true
+    addCommands {
+        command("show", "Shows current board or starts a new game if previous one is finished") {}
+        command("newgame", "Cancels the current game and starts a new one") {}
+        command("rules", "Explains the game and commands") {}
+        command("invite", "Displays an invite link for this bot") {}
+        command("help", "Alias for /rules") {}
+        command("config", "Displays a config screen") {}
+        command("setpins", "Sets the amount of pins you want to play with") {
+            int("amount", "Amount of pins. Value between 3 and 6") {
+                required = true
+            }
         }
-    }
-    addCommand(
-        "allowmultiples",
-        "Allows having multiple pins of the same color in the solution. True by default"
-    ) {
-        boolean("allow", "True to enable, False to disable") {
-            required = true
+        command(
+            "allowmultiples",
+            "Allows having multiple pins of the same color in the solution. True by default"
+        ) {
+            boolean("allow", "True to enable, False to disable") {
+                required = true
+            }
         }
     }
 
@@ -43,31 +43,15 @@ suspend fun ReadyEvent.handleReadyEvent() {
 }
 
 @KordPreview
-private suspend fun ReadyEvent.addCommand(
-    name: String,
-    description: String,
-    builder: ApplicationCommandCreateBuilder.() -> Unit = {}
-) {
-    val cmd = kord.createGlobalApplicationCommand(
-        name,
-        description,
-        builder
-    )
-    commandIds[name] = cmd.id
-    addTestCommand(name, description, builder)
+private suspend fun ReadyEvent.addCommands(builder: ApplicationCommandsCreateBuilder.() -> Unit) {
+    val commands = kord.createGlobalApplicationCommands(builder)
+    commands.collect { commandIds[it.name] = it.id }
+    addTestCommands(builder)
 }
 
 @Suppress("unused")
 @KordPreview
-private suspend fun ReadyEvent.addTestCommand(
-    name: String,
-    description: String,
-    builder: ApplicationCommandCreateBuilder.() -> Unit = {}
-) {
-    val cmd = kord.getGuild(Snowflake(661936855167664148))!!.createApplicationCommand(
-        name,
-        description,
-        builder
-    )
-    testCommandIds[name] = cmd.id
+private suspend fun ReadyEvent.addTestCommands(builder: ApplicationCommandsCreateBuilder.() -> Unit) {
+    val commands = kord.getGuild(Snowflake(661936855167664148))!!.createApplicationCommands(builder)
+    commands.collect { testCommandIds[it.name] = it.id }
 }
